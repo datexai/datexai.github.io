c3bfb4c513e46d422ee8bc3b68ff7869b67f4ff1
SULEMAN KHAN <22217732+msulemannkhan@users.noreply.github.com>
2023-01-26 12:53:46 +0500

    init

---
+++ src/sections/services.js
@@ -0,0 +1,74 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Box, Container } from 'theme-ui';
+import { rgba } from 'polished';
+import SectionHeading from 'components/section-heading';
+import Service from 'components/cards/service';
+import icon1 from 'assets/images/icons/service1.png';
+import icon2 from 'assets/images/icons/service2.png';
+import icon3 from 'assets/images/icons/service3.png';
+
+const data = [
+  {
+    id: 1,
+    icon: icon1,
+    title: 'Tailored AI',
+    description: `Customized solutions to fit your business needs.`,
+  },
+  {
+    id: 3,
+    icon: icon2,
+    title: 'Advanced Python',
+    description: `Unlock automation's potential.`,
+  },
+  {
+    id: 4,
+    icon: icon3,
+    title: 'Strategic AI',
+    description: `Expert guidance for successful implementation.`,
+  },
+];
+
+const Services = () => {
+  return (
+    <Box as="section" id="services" sx={styles.section}>
+      <Container>
+        <SectionHeading
+          sx={styles.heading}
+          title="Grow your startup with our Service"
+          description="From Python and Machine Learning software development to integration and consultation, we delivers results"
+        />
+        <Box sx={styles.contentWrapper}>
+          {data?.map((item) => (
+            <Service key={item.id} item={item} />
+          ))}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Services;
+
+const styles = {
+  section: {
+    backgroundColor: rgba('#FFF5ED', 0.5),
+    pt: [11, 11, 11, 12, 12, 12, 14],
+    pb: [7, 7, 7, 9, 9, 10, 11],
+  },
+  heading: {
+    maxWidth: [null, null, null, 455, 660],
+    mb: [6, null, null, 8, null, 9, 13],
+  },
+  contentWrapper: {
+    gap: 30,
+    display: 'grid',
+    justifyContent: ['center', null, null, 'unset'],
+    gridTemplateColumns: [
+      'repeat(1, 285px)',
+      'repeat(1, 325px)',
+      'repeat(1, 285px)',
+      'repeat(3, 1fr)',
+    ],
+  },
+};
